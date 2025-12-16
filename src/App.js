c57018e8ee8441f55482b2ef1b3df1b1b6fa5438
Vanshaj <srivastavanshaj10j@gmail.com>
2025-12-16 07:40:13 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api")
+      .then(res => res.json())
+      .then(data => setMsg(data.message));
+  }, []);
+
+  return <h1>{msg}</h1>;
+}
+
+export default App;
+
